9f962a156bab0230c7644cbe34562b3b3dc2dd62
tommy <tommy@192.168.219.108>
2024-10-24 18:19:26 +0900

    2024/10/24 프로그래머스 문자출력문제

---
+++ JAVA/src/com/practice/프로그래머스문자출력.java
@@ -0,0 +1,13 @@
+package com.practice;
+
+public class 프로그래머스문자출력 {
+	public static void main(String[] args) {	
+	int count = 3;
+	
+	while (count > 0) {
+		System.out.println(count);
+		count--;
+	}
+	System.out.println("Let's go");
+}
+}
