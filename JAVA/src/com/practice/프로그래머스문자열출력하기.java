6312650717537cb7b5b74f08de77bdeb49f6b8ad
tommy <tommy@192.168.219.108>
2024-10-25 14:12:09 +0900

    2024/10/25 프로그래머스 문자열 출력하기 scanner로 문자열 입력받아 출력해서 문제 풀었음

---
+++ JAVA/src/com/practice/프로그래머스문자열출력하기.java
@@ -0,0 +1,14 @@
+package com.practice;
+
+import java.util.Scanner;
+
+public class 프로그래머스문자열출력하기 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		System.out.println(str);
+	}
+
+}
