bf5aed6857ce4b4002d9a957d7ab0632a1d42f81
tommy <tommy@192.168.219.108>
2024-10-30 12:12:21 +0900

    2024/10/30 프로그래머스 대소문자 바꿔서 출력

---
+++ JAVA/src/com/practice/프로그래머스대소문자바꿔서출력.java
@@ -0,0 +1,18 @@
+package com.practice;
+
+import java.util.Scanner;
+
+public class 프로그래머스대소문자바꿔서출력 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		
+		str = str.toUpperCase();
+		
+		System.out.println(str);
+		
+	}
+
+}
