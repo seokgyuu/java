d912c2f56ec63929b6e99e8bbe1a1907735e7551
tommy <tommy@192.168.219.108>
2024-11-03 14:43:05 +0900

    2024/11/03 프로그래머스덧셈식출력하기

---
+++ JAVA/src/com/practice/프로그래머스덧셈식출력하기.java
@@ -0,0 +1,16 @@
+package com.practice;
+
+import java.util.Scanner;
+
+public class 프로그래머스덧셈식출력하기 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a +"+" +b + "=" + (a+b) );
+	}
+
+}
