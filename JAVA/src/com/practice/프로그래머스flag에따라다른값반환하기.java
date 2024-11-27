90eb4099c4b46b6c48bd8501171c98db9cce3e3f
tommy <tommy@192.168.219.108>
2024-11-27 11:19:47 +0900

    2024/11/27 프로그래머스 flag에 따라 다른값 반환하기

---
+++ JAVA/src/com/practice/프로그래머스flag에따라다른값반환하기.java
@@ -0,0 +1,21 @@
+package com.practice;
+
+public class 프로그래머스flag에따라다른값반환하기 {
+
+	public static void main(String[] args) {
+		int a = -4;
+		int b = 7;
+		boolean flag = true;
+		int answer = 0;
+		
+		if(flag == true ) {
+			System.out.println(a + b);
+			
+		}
+		else {
+			System.out.println(a-b);
+		}
+		return;
+	}
+
+}
